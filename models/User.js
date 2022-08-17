bcc6976e2784020c75547dacba584751e968cee7
justin <fezzygo@gmail.com>
2022-08-17 19:51:14 -0400

    updated User model (models/User.js(3-10,50-58)

--- models/User.js
+++ models/User.js
@@ -1,7 +1,13 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 User.init(
     {
@@ -40,8 +46,16 @@ User.init(
             },
         },
     },
-
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(
+                    newUserData.password,
+                    24
+                );
+                return newUserData;
+            },
+        },
         sequelize,
         freezeTableName: true,
         underscored: true,
