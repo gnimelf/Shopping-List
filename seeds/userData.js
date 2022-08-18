2fb7f85bbd252e9ec9b4f1e59c9d8638a0e42d35
justin <fezzygo@gmail.com>
2022-08-18 12:36:54 -0400

    removed user_name from seed items (seeds/userData.js:5,12,19,26,33)

--- seeds/userData.js
+++ seeds/userData.js
@@ -2,35 +2,30 @@ const  {User} = require('../models')
 
 const userData = [
     {
-        user_name: "asimone",
         first_name: "Alec",
         last_name: "Simone",
         email: "alecsimone2@gmail.com",
         password: "password",
     },
     {
-        user_name: "aali",
         first_name: "Anisa",
         last_name: "Ali",
         email: "anisa.ali.5999385@gmail.com",
         password: "password",
     },
     {
-        user_name: "tjones",
         first_name: "Taja",
         last_name: "Jones",
         email: "taja.jones2@gmail.com",
         password: "password",
     },
     {
-        user_name: "amohamud",
         first_name: "Abdul",
         last_name: "Mohamud",
         email: "AbdiganiMoham@gmail.com",
         password: "password",
     },
     {
-        user_name: "jfleming",
         first_name: "Justin",
         last_name: "Fleming",
         email: "fezzygo@gmail.com",
