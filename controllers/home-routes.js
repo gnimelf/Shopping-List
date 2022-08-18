43ee6b4f7ae8c5d4ad09a1f1ddaa06265613650a
justin <fezzygo@gmail.com>
2022-08-18 13:30:44 -0400

    added login and lists routes (controllers/home-routes.js:3-32)

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,34 @@
 const router = require("express").Router()
 
-router.get('/', async (req, res) => {
+router.get("/", (req, res) => {
     try {
-        res.render('home')
+        res.render("home")
     } catch (err) {
         console.log(err)
         res.status(500)
     }
-})
+});
+
+router.get("/login", (req, res) => {
+    try {
+        res.render("login")
+    } catch (err) {
+        console.log(err)
+        res.status(500)
+    }
+});
+
+router.get("/lists", (req, res) => {
+    if (req.session.loggedIn === true) {
+        try {
+            res.render("lists")
+        } catch (err) {
+            console.log(err)
+            res.status(500)
+        }
+    } else {
+        res.render("login")
+    }
+});
 
 module.exports = router
