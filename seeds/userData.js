b2c41b8b93c8c12289c4add819544044cab7ed50
justin <fezzygo@gmail.com>
2022-08-17 21:20:42 -0400

    created user seed (seeds/userData.js:1-35)

---
+++ seeds/userData.js
@@ -0,0 +1,35 @@
+const  {User} = require('../models')
+
+const userData = [
+    {
+        user_name: "Alec",
+        last_name: "Simone",
+        email: "alecsimone2@gmail.com",
+        password: "password",
+    },
+    {
+        user_name: "Anisa",
+        last_name: "Ali",
+        email: "anisa.ali.5999385@gmail.com",
+        password: "password",
+    },
+    {
+        user_name: "Taja",
+        last_name: "Jones",
+        email: "taja.jones2@gmail.com",
+        password: "password",
+    },
+    {
+        user_name: "Abdul",
+        last_name: "Mohamud",
+        email: "AbdiganiMoham@gmail.com",
+        password: "password",
+    },
+]
+
+const seedUser = () => User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    })
+
+module.exports = seedUser
