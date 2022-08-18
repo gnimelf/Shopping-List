f3f026787dd3bd65200d2be8f05f080bdf1b3661
justin <fezzygo@gmail.com>
2022-08-18 10:43:21 -0400

    added first_name with a value to each user

--- seeds/userData.js
+++ seeds/userData.js
@@ -2,31 +2,36 @@ const  {User} = require('../models')
 
 const userData = [
     {
-        user_name: "Alec",
+        user_name: "asimone",
+        first_name: "Alec",
         last_name: "Simone",
         email: "alecsimone2@gmail.com",
         password: "password",
     },
     {
-        user_name: "Anisa",
+        user_name: "aali",
+        first_name: "Anisa",
         last_name: "Ali",
         email: "anisa.ali.5999385@gmail.com",
         password: "password",
     },
     {
-        user_name: "Taja",
+        user_name: "tjones",
+        first_name: "Taja",
         last_name: "Jones",
         email: "taja.jones2@gmail.com",
         password: "password",
     },
     {
-        user_name: "Abdul",
+        user_name: "amohamud",
+        first_name: "Abdul",
         last_name: "Mohamud",
         email: "AbdiganiMoham@gmail.com",
         password: "password",
     },
     {
-        user_name: "Justin",
+        user_name: "jfleming",
+        first_name: "Justin",
         last_name: "Fleming",
         email: "fezzygo@gmail.com",
         password: "password",
