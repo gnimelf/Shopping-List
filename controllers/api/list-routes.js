18fc146b1f60b19993938f1ab817b5f4ca5400ae
justin <fezzygo@gmail.com>
2022-08-22 17:59:28 -0400

    added add and delete endpoints for lists routes (controller/api/list-routes.js:6-41)

--- controllers/api/list-routes.js
+++ controllers/api/list-routes.js
@@ -2,13 +2,40 @@ const router = require('express').Router()
 const { List } = require('../../models')
 
 
-// Get all lists
-
-
-// Get one list
-
-
 // Add list 
-
+router.post("/add", async (req, res) => {
+    if (req.session.loggedIn){
+        try {
+            const newList = await List.create({
+                list_name: req.body.listName,
+                user_id: req.body.listId
+            })
+            res.status(200).json(newList);
+        } catch (err) {
+            console.log(err)
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(404).end()
+    }
+})
 
 // Delete list
+router.delete("/delete", async (req, res) => {
+    if (req.session.loggedIn){
+        try {
+            const deleteList = await List.destroy({
+                where: {
+                    id: req.body.id
+                }
+            })
+            res.status(200).json(deleteList);
+        } catch (err) {
+            console.log(err)
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(404).end()
+    }
+})
+module.exports = router
